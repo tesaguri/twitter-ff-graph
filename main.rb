130055dc2d484f4c2d1724d115f9858f273a6927
Daiki Mizukami <tesaguriguma@gmail.com>
2019-12-03 04:37:05 +0900

    作業状態をファイルに保存（OOM 対策）

--- main.rb
+++ main.rb
@@ -3,13 +3,55 @@
 # Twitter 上のあるアカウントから出発し、その相互フォロー関係を幅優先でたどって辺リストを得る。
 
 require 'set'
+require 'sqlite3' # gem install sqlite3
 require 'twitter' # gem install twitter
 
 
-# 辺リストの出力先ファイル
-DUMP = 'edges.dat'
-# 探索の状態のバックアップ用ファイル
-STATE_FILE = 'state.dat'
+DB_PATH = 'db.sqlite3'
+
+is_first_run = !FileTest.exist?(DB_PATH)
+db = SQLite3::Database.new(DB_PATH)
+
+# 初回起動時にデータベースを初期化
+if is_first_run
+  begin
+    # 最初のアカウントのユーザID。コマンドライン引数から取る
+    user = Integer(ARGV[0])
+
+    db.execute <<-SQL
+      -- 辺リスト
+      CREATE TABLE edges (
+        v BIGINT NOT NULL,
+        w BIGINT NOT NULL,
+        CONSTRAINT simple UNIQUE (v, w),
+        CONSTRAINT undirected CHECK (w > v)
+      );
+    SQL
+    db.execute <<-SQL
+      -- 訪問済み頂点
+      CREATE TABLE users (
+        id INTEGER NOT NULL PRIMARY KEY,
+        -- 初期頂点からの距離
+        distance INTEGER NOT NULL
+      );
+    SQL
+    db.execute <<-SQL
+      -- 幅優先探索のキュー
+      CREATE TABLE queue (
+        -- 内部 ID。先入れ先出しを実現するために挿入タイミングの昇順に番号づけする
+        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+        user_id BIGINT
+      );
+    SQL
+
+    db.execute('INSERT INTO users (id, distance) VALUES (?, 0)', user)
+    db.execute('INSERT INTO queue (user_id) VALUES (?)', user)
+  rescue
+    db.close
+    File.delete(DB_PATH)
+    raise
+  end
+end
 
 
 # Twitter API にアクセスするためのオブジェクトを用意する
@@ -70,93 +112,55 @@ end
 
 # メイン処理
 
-# 幅優先探索の準備。
-#
-# 一般に幅優先探索では初期頂点からの距離の昇順で頂点を探索していく。
-# また、新たにキューの末尾に追加する頂点はキューの先頭から取り出す頂点の「1 歩先」であるから、
-# ある時点のキューには高々 2 通りの距離 d(v) = d-1, d の頂点しかなく、d(v) = d-1 の頂点は
-# キューの先頭側に、d(v) = d の頂点は末尾側に集まることが分かる。
-# そこで今回の探索ではこれらの境界に番兵として `nil` を置いて頂点の距離を把握する。
-#
-# キューの模式図:
-# (先頭) out <- [v_0, ..., v_m, nil, w_0, ..., w_n] <- in (末尾)
-# where
-#   d(v_i) = d - 1 (i = 0, ..., m),
-#   d(w_i) = d (i = 0, ..., n).
-
-# 最初のアカウント（頂点）のユーザID。コマンドライン引数から取る
-v = Integer(ARGV[0])
-# 幅優先探索のキュー。配列で代用している。要素の取り出しは配列の先頭から行う。一見効率が
-# 悪そうだが、実際には定数時間でできる（おそらくインタプリタの実装で工夫されているのだろう）。
-# また、今回はレートリミットの待ち時間が律速なので、仮に多少遅かったとしても無視できる。
-q = [nil, v]
-# キューの末尾の頂点の距離
-d = 0
-# 訪問済み頂点の集合
-vertices = Set.new([v])
-
-# バックアップファイルが存在するならその状態を読み込む
-if FileTest.exist?(STATE_FILE)
-  state = open(STATE_FILE) {|f| Marshal.load(f) }
-
-  q = state[:q]
-  d = state[:d]
-
-  # 頂点集合は辺リストから再現する
-  vertices = Set.new
-  open(DUMP) do |f|
-    f.each_line do |l|
-      if (m = l.match(/^(\d+)\t(\d+)(:?$|\t)/))
-        vertices.add(Integer(m[1]))
-        vertices.add(Integer(m[2]))
-      end
-    end
-  end
-end
-
-# 辺リストの出力先
-out = open(DUMP, 'a')
+# プリペアドステートメントの用意
+first_user_in_queue = db.prepare <<-SQL
+  SELECT users.id, users.distance, queue.id
+    FROM users
+    JOIN queue ON users.id == queue.user_id
+    ORDER BY queue.id ASC
+    LIMIT 1
+SQL
+delete_from_queue = db.prepare('DELETE FROM queue WHERE id == ?')
+add_edge = db.prepare('INSERT INTO edges VALUES (?, ?)')
+is_in_users = db.prepare('SELECT EXISTS (SELECT * FROM users WHERE id == ?)')
+add_user = db.prepare('INSERT INTO users (id, distance) VALUES (?, ?)')
+add_to_queue = db.prepare('INSERT INTO queue (user_id) VALUES (?)')
 
 # 探索のメインループ
-until q == [nil] do # キューが空になるまで繰り返す
-  v = q.shift # キューの先頭から頂点を取り出す
+loop do # キューが空になるまで繰り返す
+  # キューの先頭の頂点を読む（まだ取り除かない）
+  (v, d, queue_id) = first_user_in_queue.execute.first
+  break unless user # キューが空なら終了
 
-  unless v # キューから偽値（`nil`）が取り出されたとき（頂点距離の境界）
-    STDERR.puts("processed users with d = #{d-1}")
-    STDERR.puts("current queue size = #{q.length}")
-    q.push(nil)
-    # 辺リストに距離の情報をコメントとして記録
-    out.puts("\# d = #{d}")
-    d += 1
-    next
-  end
+  db.transaction do
+    # 実際にキューの先頭を削除
+    delete_from_queue.execute(queue_id)
 
-  STDERR.puts("inspecting user: #{v}, d = #{d-1}")
+    STDERR.puts("inspecting user: #{v}, d = #{d}")
 
-  # `v` の相互フォローのアカウントを集める
-  ff = begin
-    # `v` がフォローしているアカウントと `v` のフォロワーの ID を取得する
-    following = cursor_to_a { client.friend_ids(v, count: 5000) }
-    followers = cursor_to_a { client.follower_ids(v, count: 5000) }
-    # それらの共通部分をとる
-    following.value & followers.value
-  rescue Twitter::Error::Unauthorized # リクエスト失敗時
-    STDERR.puts("unauthorized request for user #{v}; maybe a protected user")
-    next
-  end
-
-  # 各相互フォロワー `w` について
-  ff.each do |w|
-    # 辺リストに出力。重複は気にしないことにする（必要ならば後処理で除く）
-    out.puts("#{v}\t#{w}")
-    unless vertices.include?(w) # 未探索ならば
-      vertices.add(w) # 頂点集合と
-      q.push(w) # キューに追加
+    # `v` の相互フォローのアカウントを集める
+    ff = begin
+      # `v` がフォローしているアカウントと `v` のフォロワーの ID を取得する
+      following = cursor_to_a { client.friend_ids(v, count: 5000) }
+      followers = cursor_to_a { client.follower_ids(v, count: 5000) }
+      # それらの共通部分をとる
+      following.value & followers.value
+    rescue Twitter::Error::Unauthorized # リクエスト失敗時
+      STDERR.puts("unauthorized request for user #{v}; maybe a protected user")
+      next
     end
-  end
 
-  # 現在の状態をバックアップに書き出す
-  open(STATE_FILE, 'w') do |f|
-    Marshal.dump({ q: q, d: d }, f)
+    # 各相互フォロワー `w` について
+    ff.each do |w|
+      # 辺リストに追加
+      begin
+        add_edge.execute(*[v, w].sort)
+      rescue SQLite3::ConstraintException
+      end
+      if is_in_users.execute(w).first == [0] # 未探索ならば
+        add_user.execute(w, d + 1) # 頂点集合と
+        add_to_queue.execute(w) # キューに追加
+      end
+    end
   end
 end
