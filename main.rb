1a08adf199348bc107c2e8f363deac6c24329a1b
Daiki Mizukami <tesaguriguma@gmail.com>
2019-12-05 15:07:39 +0900

    特定アカウントのフォロワーのフォロイーを収集するように改修
    TODO: `master` と互いの機能を損なわないように上手いこと統合する

--- main.rb
+++ main.rb
@@ -15,6 +15,7 @@
 # }
 # ````
 
+require 'securerandom'
 require 'set'
 require 'sqlite3'
 require 'twitter'
@@ -25,42 +26,32 @@ DB_PATH = 'db.sqlite3'
 is_first_run = !FileTest.exist?(DB_PATH)
 db = SQLite3::Database.new(DB_PATH)
 
+# 最初のアカウントのユーザID。コマンドライン引数から取る
+user = Integer(ARGV[0])
+
 # 初回起動時にデータベースを初期化
 if is_first_run
   begin
-    # 最初のアカウントのユーザID。コマンドライン引数から取る
-    user = Integer(ARGV[0])
-
     db.execute <<-SQL
-      -- 辺リスト
-      CREATE TABLE edges (
-        v BIGINT NOT NULL,
-        w BIGINT NOT NULL,
-        CONSTRAINT simple UNIQUE (v, w),
-        CONSTRAINT undirected CHECK (w > v)
+      -- フォロー関係
+      CREATE TABLE friendships (
+        follower BIGINT NOT NULL, -- フォロワー
+        friend BIGINT NOT NULL, -- 被フォロー
+        CONSTRAINT simple UNIQUE (follower, friend)
       );
     SQL
     db.execute <<-SQL
       -- 訪問済み頂点
       CREATE TABLE users (
         id INTEGER NOT NULL PRIMARY KEY, -- ユーザ ID
-        distance INTEGER NOT NULL, -- 初期頂点からの距離
         friends_count INTEGER, -- フォロー数
         followers_count INTEGER, -- フォロワー数
+        inspected_at INTEGER, -- フォロー・フォロワーの情報を取得した時刻のナノ秒精度の UNIX time
         accessible INTEGER -- アクセス可能かの真偽値。非公開アカウント等では `0` とする
       );
     SQL
-    db.execute <<-SQL
-      -- 幅優先探索のキュー
-      CREATE TABLE queue (
-        -- 内部 ID。先入れ先出しを実現するために挿入タイミングの昇順に番号づけする
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-        user_id BIGINT NOT NULL
-      );
-    SQL
 
-    db.execute('INSERT INTO users (id, distance) VALUES (?, 0)', user)
-    db.execute('INSERT INTO queue (user_id) VALUES (?)', user)
+    db.execute('INSERT INTO users (id) VALUES (?)', user)
   rescue
     db.close
     File.delete(DB_PATH)
@@ -79,27 +70,6 @@ end
 
 
 # ヘルパーメソッドの定義
-
-## 与えられたブロックを実行し、ブロックから返ってきた cursor から値を全て取り出して配列にまとめる。
-# また、その過程でレートリミットに到達した場合に適宜スリープしてからリトライする。
-#
-# スリープ中の時間を有効に使うために上記の処理は別スレッドで実行し、このメソッドはそのスレッドのハンドルを返す。
-#
-# # 利用例
-#
-# ```
-# handle = cursor_to_a { client.followers(user) }
-# # ...
-# followers = handle.value
-# ```
-def cursor_to_a
-  t = Thread.new do
-    cursor = catch_rate_limit { yield }
-    catch_rate_limit { cursor.to_a }
-  end
-  t.report_on_exception = false if t.respond_to?(:report_on_exception=)
-  t
-end
 
 ## レートリミットの解消を待ちながら与えられたブロックを実行する。
 def catch_rate_limit
@@ -134,67 +104,63 @@ def transaction(db)
   end
 end
 
+def now_ns
+  now = Time.now
+  now.to_i * (10 ** 9) + time.nsec
+end
+
 
 # メイン処理
 
 # プリペアドステートメントの用意
-# キューの先頭の要素をデキューせずに読む
-peek_queue = db.prepare <<-SQL
-  SELECT users.id, users.distance
-    FROM users
-    JOIN queue ON users.id == queue.user_id
-    ORDER BY queue.id ASC
-    LIMIT 1
-SQL
 set_accessibility = db.prepare <<-SQL
   UPDATE users
     SET accessible = ?2
     WHERE id = ?1
 SQL
-dequeue = db.prepare('DELETE FROM queue where id == (SELECT MIN(id) FROM queue)')
-set_friends_followers_count = db.prepare <<-SQL
-  UPDATE users
-    SET friends_count = ?2, followers_count = ?3
-    WHERE id = ?1
+add_friendship = db.prepare('INSERT OR IGNORE INTO friendships (follower, friend) VALUES (?, ?)')
+get_inspected_at = db.prepare('SELECT inspected_at FROM users where id == ?')
+add_user = db.prepare('INSERT OR IGNORE INTO users (id) VALUES (?)')
+set_inspected_at = db.prepare('UPDATE users SET inspected_at = ?2 WHERE id == ?1')
+
+if get_inspected_at.execute(user).first == [nil]
+  STDERR.puts("inspecting user: #{user}")
+  cursor = catch_rate_limit { client.follower_ids(user, count: 5000) }.each
+  loop do
+    follower = catch_rate_limit { cursor.next }
+    add_user.execute(follower)
+    add_friendship.execute(follower, user)
+  end
+  set_inspected_at.execute(user, now_ns)
+else
+  STDERR.puts("user is already inspected: #{user}")
+end
+
+followers = db.prepare(<<-SQL, user).execute.map {|(id)| id}
+  SELECT friendships.follower
+    FROM friendships
+    JOIN users ON friendships.follower == users.id
+    WHERE users.inspected_at IS NOT NULL
 SQL
-add_edge = db.prepare('REPLACE INTO edges VALUES (?, ?)')
-user_is_visited = db.prepare('SELECT EXISTS (SELECT * FROM users WHERE id == ?)')
-add_user = db.prepare('INSERT INTO users (id, distance) VALUES (?, ?)')
-enqueue = db.prepare('INSERT INTO queue (user_id) VALUES (?)')
+until followers.empty?
+  follower = followers.delete_at(SecureRandom.rand(followers.length))
 
-# 探索のメインループ
-loop do # キューが空になるまで繰り返す
-  # キューの先頭の頂点を読む（デキューは後のトランザクション内で行う）
-  (v, d) = peek_queue.execute.first
-  break unless v # キューが空なら終了
-
-  STDERR.puts("inspecting user: #{v}, d = #{d}")
-
-  # `v` のフォロー・フォロワーの ID を取得
-  (following, followers) = begin
-    following = cursor_to_a { client.friend_ids(v, count: 5000) }
-    followers = cursor_to_a { client.follower_ids(v, count: 5000) }
-    [following.value, followers.value]
-  rescue Twitter::Error::Unauthorized
-    STDERR.puts("unauthorized request for user #{v}; maybe a protected user")
-    set_accessibility.execute(v, 0)
-    dequeue.execute
-    next
-  end
-  set_accessibility.execute(v, 1)
-
-  transaction(db) do
-    dequeue.execute
-    set_friends_followers_count.execute(v, following.length, followers.length)
-
-    # 各相互フォロワーについて
-    (following & followers).each do |w|
-      add_edge.execute(*[v, w].sort)
-      if user_is_visited.execute(w).first == [0] # 未探索ならば
-        # 頂点集合とキューに追加
-        add_user.execute(w, d + 1)
-        enqueue.execute(w)
-      end
+  # XXX: 重複コード
+  STDERR.puts("inspecting user: #{follower}")
+  begin
+    cursor = catch_rate_limit { client.friend_ids(follower, count: 5000) }.each
+    loop do
+      friend = catch_rate_limit { cursor.next }
+      add_user.execute(friend)
+      add_friendship.execute(follower, friend)
     end
+  rescue Twitter::Error::Unauthorized
+    STDERR.puts("unauthorized request for user #{follower}; maybe a protected user")
+    set_accessibility.execute(follower, 0)
+    next
+  rescue Twitter::Error::NotFound
+    STDERR.puts("user has been deleted: #{user}")
+    set_accessibility.execute(follower, 0)
   end
+  set_inspected_at.execute(user, now_ns)
 end
