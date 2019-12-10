5e676c6471c2ec2efbc29bd51d3d3413d9fe2bc9
Daiki Mizukami <tesaguriguma@gmail.com>
2019-12-11 01:36:56 +0900

    フォローを収集する対象のフォロワーの抽出を簡略化

--- main.rb
+++ main.rb
@@ -129,24 +129,12 @@ add_friendship = db.prepare('INSERT OR IGNORE INTO friendships (follower, friend
 add_user = db.prepare('INSERT OR IGNORE INTO users (id) VALUES (?)')
 set_got_followers_at = db.prepare('UPDATE users SET got_followers_at = ?2 WHERE id == ?1')
 set_got_friends_at = db.prepare('UPDATE users SET got_friends_at = ?2 WHERE id == ?1')
-# 各ターゲットについての情報を得る
-target_status = db.prepare <<-SQL
-  SELECT
-      targets.id,
-      (SELECT COUNT() FROM friendships WHERE friend == targets.id), -- ターゲットのフォロワー数
-      ( -- ターゲットのフォロワーのうちフォローが未取得であるものの数
-        SELECT COUNT() FROM friendships
-          JOIN users ON users.id == follower
-          WHERE friend == targets.id AND users.got_friends_at IS NULL
-      )
-    FROM targets
-SQL
-# 指定されたアカウントのフォロワーのうち、そのフォローが未取得であるものの中から、指定された位置にあるものを返す
-uninspected_follower_of_at = db.prepare <<-SQL
-  SELECT follower
+# ターゲットのフォロワーのうち、そのフォローが未収集であるものの中から、指定された位置にあるものを返す
+uninspected_follower_at = db.prepare <<-SQL
+  SELECT DISTINCT follower
     FROM friendships
     JOIN users ON follower == users.id
-    WHERE friend == ? AND users.got_friends_at IS NULL
+    WHERE friend IN targets AND users.got_friends_at IS NULL
     LIMIT ?, 1
 SQL
 
@@ -158,7 +146,7 @@ SQL
 
 targets.each do |(user, got_followers_at)|
   if !got_followers_at # フォロワーが未収集
-    STDERR.puts("inspecting user: #{user}")
+    STDERR.puts("inspecting target: #{user}")
     cursor = catch_rate_limit { client.follower_ids(user, count: 5000) }.each
     loop do
       follower = catch_rate_limit { cursor.next }
@@ -167,52 +155,40 @@ targets.each do |(user, got_followers_at)|
     end
     set_got_followers_at.execute(user, now_ns)
   else
-    STDERR.puts("user is already inspected: #{user}")
+    STDERR.puts("target is already inspected: #{user}")
   end
 end
 
+uninspected_count = db.execute(<<-SQL).first.first
+  SELECT COUNT(DISTINCT follower)
+    FROM friendships
+    JOIN users ON follower == users.id
+    WHERE friend IN targets AND users.got_friends_at IS NULL
+SQL
+
 # ターゲットのフォロワーのフォローの収集
-loop do
-  status = target_status.execute
+until uninspected_count == 0
+  # フォローが未収集であるフォロワーを無作為にとる
+  follower = uninspected_follower_at.execute(SecureRandom.random_number(uninspected_count)).first.first
 
-  # 既に全てのフォロワーのフォローを取得していれば終了
-  if status.all? {|(_, uninspected_count)| uninspected_count == 0 }
-    STDERR.puts("finished all the tasks")
-    break
-  end
-
-  # ターゲットのうち、そのフォロワーのうちフォローが未収集なものが占める割合が最大であるものを取る
-  (user, _, uninspected_count) = status.max_by do |(_, followers_count, uninspected_count)|
-    uninspected_count.to_f / followers_count
-  end
-
-  # `user` のフォロワーのフォローを 15 件（レートリミットの件数と同じ）取得する
-  # 15 件ずつにまとめているのは、`target_status` の処理が重くて 1 件ごとに実行するのが非効率なため
-  15.times do
-    break if uninspected_count == 0
-
-    # フォローが未取得であるフォロワーをランダムに取る
-    follower = uninspected_follower_of_at.execute(user, SecureRandom.random_number(uninspected_count)).first.first
-
-    # XXX: 重複コード
-    STDERR.puts("inspecting user: #{follower}")
-    begin
-      cursor = catch_rate_limit { client.friend_ids(follower, count: 5000) }.each
-      loop do
-        friend = catch_rate_limit { cursor.next }
-        add_user.execute(friend)
-        add_friendship.execute(follower, friend)
-      end
-    rescue Twitter::Error::Unauthorized
-      STDERR.puts("unauthorized request for user #{follower}; maybe a protected user")
-      set_accessibility.execute(follower, 0)
-      next
-    rescue Twitter::Error::NotFound
-      STDERR.puts("user has been deleted: #{user}")
-      set_accessibility.execute(follower, 0)
+  # XXX: 重複コード
+  STDERR.puts("inspecting follower: #{follower}")
+  begin
+    cursor = catch_rate_limit { client.friend_ids(follower, count: 5000) }.each
+    loop do
+      friend = catch_rate_limit { cursor.next }
+      add_user.execute(friend)
+      add_friendship.execute(follower, friend)
     end
-
-    set_got_friends_at.execute(follower, now_ns)
-    uninspected_count -= 1
+  rescue Twitter::Error::Unauthorized
+    STDERR.puts("unauthorized request for user #{follower}; maybe a protected user")
+    set_accessibility.execute(follower, 0)
+    next
+  rescue Twitter::Error::NotFound
+    STDERR.puts("user has been deleted: #{user}")
+    set_accessibility.execute(follower, 0)
   end
+
+  set_got_friends_at.execute(follower, now_ns)
+  uninspected_count -= 1
 end
